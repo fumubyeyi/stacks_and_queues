b18b1b20a7c389db85faa923c56814e580e807e4
fumubyeyi <iragaba.umubyeyi@gmail.com>
2023-08-28 20:54:42 -0400

    Added a class for implementing stack functions

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # stacks_and_queues
 Learn Stack and Queue implementation
+
+## Includes
+- Stack: library that implement a Stack with pop(), push(), peek(), and isEmpty() functions.
+- Queue: library that implement a Queue with enqueue(), dequeue(), and isEmpty() functions.
+- StackAndQueueApp: a console app that references the libraries and makes call to their functions.
+
+## Reference
+CRACKING the CODING INTERVIEW by Gayle Laakmann McDowell
+
---
+++ Stack/MyStack.cs
@@ -0,0 +1,32 @@
+﻿namespace Stack;
+public class MyStack
+{
+    public static Node<T>{
+        public T data;
+        public Node<T> next;
+
+        public Node(T data){
+            this.data = data;
+        }
+        public Node<T> top;
+
+        public T pop(){
+            if (top == null) throw new EmptyStackException();
+            T item = top.data;
+            top = top.next;
+            return item;
+        }
+        public void push(T item){
+            Node<T> newNode = new(item);
+            newNode.next = top;
+            top = newNode;
+        }
+        public T peek(){
+            return top.data;
+        }
+        public bool isEmpty(){
+            return top == null;
+        }
+
+    }
+}
